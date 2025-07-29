1843bc62cfcbf7a0abcbcdead43d950a786ad66d
Carlos Medina <carlosdavidmedinarangel1007@gmail.com>
2025-07-29 18:29:51 -0400

    Proyecto

---
+++ test_finetune.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestFinetune(unittest.TestCase):
+    def test_training_process(self):
+        self.assertTrue(True)
+
+    def test_model_predictions(self):
+        self.assertEqual(1, 1)
+
+if __name__ == '__main__':
+    unittest.main()
